8a967938d2c6401daf1e87a546445209c8a1a176
andypayne <apayne@gmail.com>
2020-05-05 19:29:38 -0700

    Notes

--- lst/type_to_string.hpp
+++ lst/type_to_string.hpp
@@ -1,5 +1,8 @@
 #include <string>
 
+// TODO: Consider using the namespace technique described here:
+// https://stackoverflow.com/a/37425306
+
 template <typename T>
 std::string type_to_string(T item) {
   return std::to_string(item);
